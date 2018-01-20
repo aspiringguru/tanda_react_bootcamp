c1c1a7e39250b8743abf740ae124fa3a056642f8
aspiringguru <bmatthewtaylor@gmail.com>
2018-01-20 13:39:41 +1000

    chapter 3 + misc demos

--- source/Component/Article/index.jsx
+++ source/Component/Article/index.jsx
@@ -1,9 +1,14 @@
+import React from 'react';
+
 export default function Article(props) {
   return (
-    <div>
-      <h1>{props.title}</h1>
-      <p>{props.description}</p>
-      <a href={props.link}>{props.link}</a>
-    </div>
+    <article>
+      <h3> {props.title} </h3>
+      <h2> {props.precis} </h2>
+      <p>
+        {props.description}
+        <a href={props.link}> {props.link} </a>
+      </p>
+    </article>
   )
 }
--- source/index.js
+++ source/index.js
@@ -3,12 +3,58 @@ import ReactDom from "react-dom";
 
 import HelloWorld2 from "./Component/HelloWorld";
 import HelloPluto from "./Component/HelloPluto";
+import Article from "./Component/Article"
+
+function NewsFeed(props) {
+  const articles = props.articles.map(article =>
+    <Article
+      description={article.description}
+      precis={article.precis}
+      key={article.id}
+      link={article.link}
+      title={article.title}
+    />
+  )
+
+  return (
+    <div>{articles}</div>
+  )
+}
+
+const LOREM_IPSUM = 'blah blah eerg blah zzz'
+const MY_PRECIS = 'awesomeness'
+
+const ARTICLES = [
+  {
+    description: LOREM_IPSUM,
+    precis: MY_PRECIS,
+    id: 1,
+    link: 'https://google.com',
+    title: 'Article 1',
+  },
+  {
+    description: LOREM_IPSUM,
+    precis: MY_PRECIS,
+    id: 2,
+    link: 'https://google.com',
+    title: 'Article 2',
+  },
+  {
+    description: LOREM_IPSUM,
+    precis: MY_PRECIS,
+    id: 3,
+    link: 'https://google.com',
+    title: 'Article 3',
+  },
+]
+
 
 
 const VIEW =
   <div>
     <HelloWorld2/>
     <HelloPluto/>
+    <NewsFeed articles={ARTICLES} />
   </div>
 
 ReactDom.render(
